baf976999df96af0e0adcc4eb568608e83e9b2b3
Medger Deana <medeana@at-n3.msk.21-school.ru>
2022-08-11 11:25:35 +0300

    done

--- Calculator/ex02/ViewController.swift
+++ Calculator/ex02/ViewController.swift
@@ -8,11 +8,13 @@ class ViewController: UIViewController {
         
     }
 
+    @IBOutlet weak var error: UILabel!
     @IBOutlet weak var label: UILabel!
-    var numberFromScreen:Double = 0;
-    var firstNum:Double = 0;
-    var operation:Int = 0;
-    var mathSign:Bool = false;
+    
+    var numberFromScreen:Double = 0
+    var firstNum:Double = 0
+    var operation:Int = 0
+    var mathSign:Bool = false
     
     @IBAction func digits(_ sender: UIButton) {
         
@@ -29,20 +31,20 @@ class ViewController: UIViewController {
     
     @IBAction func buttons(_ sender: UIButton) {
         if label.text != "" && sender.tag != 10
-            && sender.tag != 11 && sender.tag != 12 {
+            && sender.tag != 11 && sender.tag != 12  && mathSign == false{
             
             firstNum = Double(label.text!)!
             
-            if sender.tag == 16 { // деление
+            if sender.tag == 16 {// деление
                 label.text = "/";
             }
-            else if sender.tag == 15 { // умножение
+            else if sender.tag == 15 {// умножение
                 label.text = "*";
             }
-            else if sender.tag == 14 { // вычет
+            else if sender.tag == 14 {// вычет
                 label.text = "-";
             }
-            else if sender.tag == 13 { // слож
+            else if sender.tag == 13 {// слож
                 label.text = "+";
             }
             operation = sender.tag
@@ -51,26 +53,44 @@ class ViewController: UIViewController {
         }
         else if sender.tag == 12 {
             if operation == 16 {
-                label.text = String(firstNum / numberFromScreen)
+                if numberFromScreen != 0 {
+                    firstNum = firstNum / numberFromScreen
+                    label.text = String(firstNum)
+                }
+                else {
+                    label.text = ""
+                }
             }
             else if operation == 15 {
-                label.text = String(firstNum * numberFromScreen)
+                firstNum = firstNum * numberFromScreen
+                label.text = String(firstNum)
             }
             else if operation == 14 {
-                label.text = String(firstNum - numberFromScreen)
+                firstNum = firstNum - numberFromScreen
+                label.text = String(firstNum)
             }
             else if operation == 13 {
-                label.text = String(firstNum + numberFromScreen)
+                firstNum = firstNum + numberFromScreen
+                label.text = String(firstNum)
             }
         }
         else if sender.tag == 10 {
             label.text = ""
             firstNum = 0
             numberFromScreen = 0
-            operation = 0 
+            operation = 0
+        }
+        else if sender.tag == 11 {
+            numberFromScreen = Double(label.text!)!
+            if numberFromScreen > 0 {
+                label.text = "-" + String(numberFromScreen)
+                numberFromScreen *= -1;
+            }
+            else if numberFromScreen < 0 {
+                numberFromScreen *= -1
+                label.text = String(numberFromScreen)
+            }
         }
     }
-    
-    
 }
 
